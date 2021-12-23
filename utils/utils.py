69942ba7489208f235b36986a7df64f60caf6ba8
O1Si <45961392+O1Si@users.noreply.github.com>
2021-12-24 00:03:25 +0800

    imp: refactor check_config function

--- utils/utils.py
+++ utils/utils.py
@@ -91,13 +91,13 @@ def s_log(flag):
 def check_config(config: dict) -> bool:
     if config.get('accounts'):
         for i in config.get('accounts'):
-            if i.get('uid') == '' or i.get('uid') is None:
+            if i.get('uid') == '' or not i.get('uid'):
                 return False
-            if i.get('password') == '' or i.get('password') is None:
+            if i.get('password') == '' or not i.get('password'):
                 return False
-            if i.get('user-agent') == '' or i.get('user-agent') is None:
+            if i.get('user-agent') == '' or not i.get('user-agent'):
                 return False
-            if i.get('board-id') == '' or i.get('board-id') is None:
+            if i.get('board-id') == '' or not i.get('board-id'):
                 return False
             if not isinstance(i.get('check-in'), bool):
                 return False
