4185dfd61cbad14b946e99e1fa09670801e70ab1
PianCat <50688679+PianCat@users.noreply.github.com>
2023-11-14 17:28:12 +0800

    添加禁用任务提醒 (#150)
    添加禁用任务提醒

--- firstrun.sh
+++ firstrun.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 # new Env("MIUI-Auto-Task 环境配置")
-# cron 0 0 1 * * firstrun.sh
-
+echo "在本任务运行完后请不要忘记禁用该任务！"
+echo "在本任务运行完后请不要忘记禁用该任务！"
+echo "在本任务运行完后请不要忘记禁用该任务！"
+echo "————————————"
 echo "开始安装依赖"
 pip install -i https://mirrors.aliyun.com/pypi/simple/ -r /ql/data/repo/0-8-4_miui-auto-tasks_master/requirements.txt
 echo "依赖已安装完毕"
-
+echo "————————————"
 echo "开始首次执行"
 task 0-8-4_miui-auto-tasks_master/miuitask.py
 echo "首次执行完毕"
+echo "————————————"
+echo "请不要忘记禁用该任务！"
+echo "请不要忘记禁用该任务！"
+echo "请不要忘记禁用该任务！"
 echo "请到 脚本管理 - 0-8-4_miui-auto-tasks_master - data - config.yml 中配置参数"
+echo "————————————"
