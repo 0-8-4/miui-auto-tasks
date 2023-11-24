21b6c4bad36a92119251ea4671c1796b974f34a5
Night-stars-1 <nujj1042633805@gmail.com>
2023-11-24 17:40:47 +0800

    chore: 更改日志输出样式

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,7 @@
 '''
 Date: 2023-11-12 14:05:06
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
-LastEditTime: 2023-11-24 13:10:39
+LastEditTime: 2023-11-24 17:39:16
 '''
 import os
 import sys
@@ -27,12 +27,14 @@ log = logger
 log.remove()
 
 log.add(sys.stdout, level="INFO", colorize=True,
-        format="<cyan>{module}</cyan>.<cyan>{function}</cyan>"
+        format="<green>{time:HH:mm:ss}</green> - "
+               "<level>{level: <5}</level> | "
+               "<cyan>{module}</cyan>.<cyan>{function}</cyan>"
                ":<cyan>{line}</cyan> - "
                "<level>{message}</level>", filter=InterceptHandler)
 
 log.add(path_log, level="DEBUG",
         format="{time:HH:mm:ss} - "
-               "{level}\t| "
+               "{level: <5} | "
                "{module}.{function}:{line} - {message}",
         rotation="1 days", enqueue=True, serialize=False, encoding="utf-8", retention="10 days")
