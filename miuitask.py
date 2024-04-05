cc79fc808fe1af254095cd400ece881dc68f9bef
Night-stars-1 <nujj1042633805@gmail.com>
2024-04-05 22:51:59 +0800

    modified:   miuitask.py

--- miuitask.py
+++ miuitask.py
@@ -1,14 +1,10 @@
 """
 Author: Night-stars-1 nujj1042633805@gmail.com
 Date: 2024-02-21 22:50:45
-LastEditTime: 2024-04-05 22:30:09
+LastEditTime: 2024-04-05 22:43:45
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
 """
-'''
-Date: 2023-11-13 20:29:19
-LastEditors: Night-stars-1 nujj1042633805@gmail.com
-LastEditTime: 2023-12-31 01:26:22
-'''
+
 # new Env("MIUI-Auto-Task") # pylint: disable=missing-module-docstring
 # cron 30 8 * * * miuitask.py
 
@@ -45,13 +41,19 @@ async def main():
                             if task.showType:
                                 log.info(f"{task.name}任务已完成")
                                 continue
-                            if not (task_obj := sign_task_obj.get(task.name)): # 签到任务对象
+                            if not (
+                                task_obj := sign_task_obj.get(task.name)
+                            ):  # 签到任务对象
                                 log.error(f"未找到{task.name}任务")
                                 continue
                             if not getattr(account, task_obj.__name__):
                                 log.info(f"任务{task.name}被禁用")
                                 continue
-                            token = await get_token(cookies["cUserId"]) if task_obj == CheckIn else None
+                            token = (
+                                await get_token(cookies["cUserId"])
+                                if task_obj == CheckIn
+                                else None
+                            )
                             status, reason = await task_obj(account, token).sign()
                             if not status and reason == "cookie":
                                 raise ValueError("Cookie失效")
