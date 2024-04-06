08ddd7c66ff55d532eb7fda552c28b8f5fd76011
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2024-04-06 14:58:18 +0800

    feat: 输出二维码登录链接，显示+1概率 (#268)
    * feat: 输出二维码登录链接，显示+1概率
    
    * modified:   utils/system_info.py
    
    * modified:   miuitask.py
    
    * modified:   requirements.txt
    modified:   utils/api/login.py
    
    * modified:   utils/api/login.py
    
    * Update Dockerfile
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 0-8-4 <ljd69154@liangjundi.cn>

--- miuitask.py
+++ miuitask.py
@@ -1,14 +1,16 @@
-'''
-Date: 2023-11-13 20:29:19
+"""
+Author: Night-stars-1 nujj1042633805@gmail.com
+Date: 2024-02-21 22:50:45
+LastEditTime: 2024-04-05 22:43:45
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
-LastEditTime: 2023-12-18 19:21:36
-'''
+"""
+
 # new Env("MIUI-Auto-Task") # pylint: disable=missing-module-docstring
 # cron 30 8 * * * miuitask.py
 
 import asyncio
 
-from tenacity import RetryError, Retrying, stop_after_attempt
+from tenacity import Retrying, stop_after_attempt
 
 from utils.api.login import Login
 from utils.api.sign import BaseSign, CheckIn
@@ -30,7 +32,8 @@ async def main():
                 with attempt:
                     login_obj = Login(account)
                     if cookies := await login_obj.login():
-                        sign_obj = BaseSign(cookies, account.user_agent)
+                        await login_obj.checkin_info()
+                        sign_obj = BaseSign(account)
                         daily_tasks = await sign_obj.check_daily_tasks()
                         sign_task_obj = sign_obj.AVAILABLE_SIGNS  # 签到任务对象合集
                         for task in daily_tasks:
@@ -38,19 +41,25 @@ async def main():
                             if task.showType:
                                 log.info(f"{task.name}任务已完成")
                                 continue
-                            if not (task_obj := sign_task_obj.get(task.name)): # 签到任务对象
+                            if not (
+                                task_obj := sign_task_obj.get(task.name)
+                            ):  # 签到任务对象
                                 log.error(f"未找到{task.name}任务")
                                 continue
                             if not getattr(account, task_obj.__name__):
                                 log.info(f"任务{task.name}被禁用")
                                 continue
-                            token = await get_token(cookies["cUserId"]) if task_obj == CheckIn else None
-                            status, reason = await task_obj(cookies, account.user_agent, token).sign()
+                            token = (
+                                await get_token(cookies["cUserId"])
+                                if task_obj == CheckIn
+                                else None
+                            )
+                            status, reason = await task_obj(account, token).sign()
                             if not status and reason == "cookie":
                                 raise ValueError("Cookie失效")
                         user_info = await sign_obj.user_info()
                         log.info(f"{user_info.title} 成长值: {user_info.point}")
-        except RetryError:
+        except ValueError:
             ...
     notify_me(InterceptHandler.message)
 
